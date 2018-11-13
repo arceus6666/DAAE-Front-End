71858c165b9ee2d8a5abb7835c6f424a03cc634b
Daniel Mendoza <arceus6666@hotmail.com>
2018-11-13 16:30:16 -0400

    generate error, home, login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule
