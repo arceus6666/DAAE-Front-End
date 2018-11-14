d676e2bee5c2f864d554c363fde9f1b69fc73a01
Daniel Mendoza <arceus6666@hotmail.com>
2018-11-14 15:59:50 -0400

    app.module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RestapiService } from './services/restapi.service';
+import { LoggerService } from './services/logger.service';
+import { appRoutingProviders, routing } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -16,9 +21,16 @@ import { LogoutComponent } from './logout/logout.component';
     LogoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    RestapiService,
+    LoggerService,
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
