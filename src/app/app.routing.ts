663b46d0a0db70fa5fbb9ea46f54533b501e75d7
Daniel Mendoza <arceus6666@hotmail.com>
2018-11-18 18:19:35 -0400

    merge

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from "./home/home.component"
 import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
   { path: '**', component: ErrorComponent }
