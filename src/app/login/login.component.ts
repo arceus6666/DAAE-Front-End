931a2020d18549f11dcb19bf3f12fe5eb2284f18
Daniel Mendoza <arceus6666@hotmail.com>
2018-11-18 17:46:38 -0400

    update login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestapiService } from '../services/restapi.service';
+import { LoggerService } from '../services/logger.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  private mydata: any
+  private code: number
+  private password: string
+  constructor(
+    private _service: RestapiService,
+    private _logger: LoggerService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this._service.getGlobal('/users/login', '', this.code + ' ' + this.password).subscribe(data => {
+      this.mydata = data
+      this._logger.setRole(this.mydata.role)
+      this._logger.setID(this.mydata._id)
+    }, error => {
+      console.log(error)
+    })
+    this._logger.logIn()
+    this._router.navigate(['home'])
   }
 
 }
