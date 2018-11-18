3c27a732dac5145d24d41db2311fb436c5456511
EValeriaAliagaTaborga <valealiaga2121@gmail.com>
2018-11-18 18:09:57 -0400

    Profile base

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,15 @@
-import { RouterModule, Routes } from '@angular/router'
-import { ModuleWithProviders } from '@angular/core'
-import { HomeComponent } from "./home/home.component"
+import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { HomeComponent } from "./home/home.component";
 import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import {ProfileComponent} from './profile/profile.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
   { path: '**', component: ErrorComponent }
