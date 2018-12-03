6f4948a742ef257761ea1b22b3c17f2781c9e24a
Daniel Mendoza <arceus6666@hotmail.com>
2018-12-03 03:05:58 -0400

    more fixes

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -36,6 +36,41 @@ export class SearchFormComponent implements OnInit {
     })
   }
 
-  getCareer
+  getCareer(id) {
+    switch (id) {
+      case 'admin':
+        return 'Administración de Empresas'
+      case 'arqui':
+        return 'Arquitectura'
+      case 'comu':
+        return 'Comunicación'
+      case 'derecho':
+        return 'Derecho'
+      case 'disgraf':
+        return 'Diseño Gráfico'
+      case 'economia':
+        return 'Economía'
+      case 'civil':
+        return 'Ingeniería Civil'
+      case 'comercial':
+        return 'Ingeniería Comercial'
+      case 'isc':
+        return 'Ingeniería de Sistemas Computacionales'
+      case 'electro':
+        return 'Ingeniería Electromecánica'
+      case 'telecom':
+        return 'Ingeniería en Sistemas Electrónicos y de Telecomunicaciones'
+      case 'financiera':
+        return 'Ingeniería Financiera'
+      case 'industrial':
+        return 'Ingeniería Industrial y de Sistemas'
+      case 'petrolera':
+        return 'Ingeniería Petrolera'
+      case 'produccion':
+        return 'Ingeniería de Producción'
+      case 'mkt':
+        return 'Marketing y Logística'
+    }
+  }
 
 }
