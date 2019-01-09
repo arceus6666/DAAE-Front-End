3395142e482d31bab4fb3bf2d7bbe03f24465ce3
Daniel Mendoza <arceus6666@hotmail.com>
2019-01-09 08:24:40 -0400

    searchs design

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,10 +5,11 @@ import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ProfileComponent } from './profile/profile.component';
-import {SearchFormComponent} from './search-form/search-form.component';
-import {SearchStudentComponent} from './search-student/search-student.component';
-import {ViewFormComponent} from './view-form/view-form.component';
-import {AdminConfigComponent} from './admin-config/admin-config.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { SearchStudentComponent } from './search-student/search-student.component';
+import { ViewFormComponent } from './view-form/view-form.component';
+import { AdminConfigComponent } from './admin-config/admin-config.component';
+import { InsertFormComponent } from './insert-form/insert-form.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,8 +17,9 @@ const appRoutes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'search-form', component: SearchFormComponent },
   { path: 'search-student', component: SearchStudentComponent },
-  { path: 'view-form', component: ViewFormComponent},
-  { path: 'admin-config', component: AdminConfigComponent},
+  { path: 'view-form', component: ViewFormComponent },
+  { path: 'insert-form', component: InsertFormComponent },
+  { path: 'admin-config', component: AdminConfigComponent },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
   { path: '**', component: ErrorComponent }
