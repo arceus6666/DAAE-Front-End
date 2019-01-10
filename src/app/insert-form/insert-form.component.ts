323580c97ebf9ae01afee5c514c75d479b86ea46
Daniel Mendoza <arceus6666@hotmail.com>
2019-01-10 04:08:33 -0400

    insert forms

--- src/app/insert-form/insert-form.component.ts
+++ src/app/insert-form/insert-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RestapiService } from '../services/restapi.service';
+import { LoggerService } from '../services/logger.service';
+import { Router } from '@angular/router';
+import { StuffManagerService } from '../services/stuff-manager.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-insert-form',
@@ -7,9 +13,95 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsertFormComponent implements OnInit {
 
-  constructor() { }
+  private form: any;
+  private err1: boolean;
+  private err2: boolean;
+  private f: FormGroup;
+  private ftype;
+  private imageString: string;
+
+  constructor(
+    public _restapi: RestapiService,
+    public _logger: LoggerService,
+    public _stuffManager: StuffManagerService,
+    private _router: Router,
+    private _builder: FormBuilder,
+    private _cleaner: DomSanitizer
+  ) {
+    this.form = { examen_de_suficiencia: false, materia_incompleta: false };
+    this.err1 = false;
+    this.err2 = false;
+    this.f = this._builder.group({
+      name: ['image', Validators.required],
+      file: null
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onFileChange(event) {
+    let reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.ftype = file.type;
+        this.f.get('file').setValue({
+          filename: 'image',
+          filetype: file.type,
+          value: (reader.result).toString().split(',')[1]
+        })
+      };
+    }
+  }
+
+  register() {
+    this.imageString = 'data:image/png;base64, ' + this.f.value.file.value;
+    this.form.digital = {
+      obj: { value: this.f.value, ftype: this.ftype },
+      is: true
+    };
+    if (this.form.category === 'materiaIncompleta') {
+      this.form.materia_incompleta.is = true;
+    }
+    if (this.form.category === 'examenSuficiencia') {
+      this.form.examen_de_suficiencia.is = true;
+    }
+    this._restapi.getGlobal(['forms', 'student-code', this.form.student_code]).subscribe(data => {
+      let mdata: any = data;
+      let q = 0;
+      if (this.form.category === 'materiaIncompleta') {
+        for (let i in mdata) {
+          if (mdata[i].materia_incompleta) {
+            q++;
+          }
+        }
+        if (q < 3) {
+          this._restapi.postGlobal(['forms', 'register'], this.form).subscribe(data => {
+
+          }, err => {
+            console.log(err)
+          })
+        }
+      }
+      if (this.form.category === 'examenSuficiencia') {
+        for (let i in mdata) {
+          if (mdata[i].examen_de_suficiencia) {
+            q++;
+          }
+        }
+        if (q < 11) {
+          this._restapi.postGlobal(['forms', 'register'], this.form).subscribe(data => {
+
+          }, err => {
+            console.log(err)
+          })
+        }
+      }
+    }, err => {
+      console.log(err)
+    })
   }
 
 }
--- src/app/services/stuff-manager.service.ts
+++ src/app/services/stuff-manager.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StuffManagerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   /**
    * Save an item in local storage.
@@ -31,5 +33,30 @@ export class StuffManagerService {
    */
   public removeItem(key: string): void {
     localStorage.removeItem(key);
+  }
+
+  public upload(files: Set<File>): { [key: string]: Observable<number> } {
+    const status = {};
+    console.log(files)
+    files.forEach(file => {
+      const formData: FormData = new FormData();
+      formData.append('file', file, file.name);
+      const req = new HttpRequest('POST', 'http://localhost:7890/upload', formData, {
+        reportProgress: true
+      });
+      const progress = new Subject<number>();
+      this.http.request(req).subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          const percentDone = Math.round(100 * event.loaded / event.total);
+          progress.next(percentDone);
+        } else if (event instanceof HttpResponse) {
+          progress.complete();
+        }
+      });
+      status[file.name] = {
+        progress: progress.asObservable()
+      };
+    });
+    return status;
   }
 }
